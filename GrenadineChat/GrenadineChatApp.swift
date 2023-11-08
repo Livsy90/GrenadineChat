4d4de8cb3ecab386409316ad825b30755d07d925
Livsy90 <artmir@live.ru>
2023-11-08 21:22:45 +0400

    Initial Commit

---
+++ GrenadineChat/GrenadineChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrenadineChatApp.swift
+//  GrenadineChat
+//
+//  Created by Livsy on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GrenadineChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
